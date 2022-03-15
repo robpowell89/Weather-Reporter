75f79b39238b03c2883241ecc3cfc4edc067c03b
Robert Powell <90731739+robpowell89@users.noreply.github.com>
2022-03-14 23:29:19 -0400

    Update weather reporter.py
    Second Commit on 3/14/22; the basic layout of project is finished; will add some more advanced stuff here soon

--- weather reporter.py
+++ weather reporter.py
@@ -1,7 +1,12 @@
 import requests
 from pprint import pprint
 
-API_Key = ''
+API_Key = '44935b268afa92473923a872ef6c0197'
 
 city = input("Enter a city: ")
 
+base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
+
+weather_data = requests.get(base_url).json()
+
+pprint(weather_data)
